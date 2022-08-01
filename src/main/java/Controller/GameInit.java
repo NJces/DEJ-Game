ae45eb4b6c43a731f3224a0ae6e5b9cb08d768d2
Narges <NJcesFUM@gmail.com>
2022-08-01 17:06:01 +0430

    fixed graphic(number of players)

---
+++ src/main/java/Controller/GameInit.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameInit implements Initializable {
+    @FXML
+    private Button boostBtn;
+    @FXML
+    private Button reduceBtn;
+    @FXML
+    private Label player_NumTxt;
+    @FXML
+    private Button submitBtn;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
