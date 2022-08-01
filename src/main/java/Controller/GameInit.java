d2d5148af138fa811ddddfa5de3e4c69067231ed
Narges <NJcesFUM@gmail.com>
2022-08-01 17:58:59 +0430

    fixed boost & reduce(number of players)

--- src/main/java/Controller/GameInit.java
+++ src/main/java/Controller/GameInit.java
@@ -1,16 +1,22 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseDragEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameInit implements Initializable {
+    @FXML
+    private ImageInput upFlash;
+    @FXML
+    private ImageInput downFlash;
     @FXML
     private Button boostBtn;
     @FXML
@@ -23,5 +29,28 @@ public class GameInit implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void boost(ActionEvent actionEvent) {
+        if (player_NumTxt.getText().charAt(0) < '7') {
+            downFlash.setSource(new Image("/Image/RedDownFlash.png"));
+            upFlash.setSource(new Image("/Image/BlueUpFlash.png"));
+            int number = player_NumTxt.getText().charAt(0) - '0';
+            player_NumTxt.setText(number + 1 + " ");
+        }
+    }
+
+    public void radius(ActionEvent actionEvent) {
+        if (player_NumTxt.getText().charAt(0) > '2') {
+            upFlash.setSource(new Image("/Image/RedUpFlash.png"));
+            downFlash.setSource(new Image("/Image/BlueDownFlash.png"));
+            int number = player_NumTxt.getText().charAt(0) - '0';
+            player_NumTxt.setText(number - 1 + " ");
+        }
+    }
+
+    public void showPlayerList(ActionEvent actionEvent) {
+        upFlash.setSource(new Image("/Image/RedUpFlash.png"));
+        downFlash.setSource(new Image("/Image/RedDownFlash.png"));
     }
 }
